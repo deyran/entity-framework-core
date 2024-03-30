60fb2fde1d0d8230c02a38dbb5a3c9770eee69a8
Deyvid Rannyere <deyran@gmail.com>
2024-03-30 11:29:22 -0300

    update

---
+++ EntityFrameWorkCore/Data/ContosoPizzaContext.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameWorkCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameWorkCore.Data
+{
+    public class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"Data Source=(localdb)\\MSSQLLocalDB; Database=ContosoPizzaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ EntityFrameWorkCore/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace EntityFrameWorkCore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderFulfilled { get; set; }
+        public int Customer { get; set; }
+
+        public Customer Customers { get; set; } = null!;
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
