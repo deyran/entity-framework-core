9ac16fc6db1f0bdbf9419bf340bd0026d1c1627f
Deyvid Rannyere <deyran@gmail.com>
2024-05-13 20:34:14 -0300

    Update

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -5,28 +5,48 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        // using ContosoPizzaContext context = new ContosoPizzaContext();
+
+        // var veggieSpecial = context.Products
+        //                         .Where(p => p.Name == "Veggie Special Pizza")
+        //                         .FirstOrDefault();
+
+        // if (veggieSpecial is Product)
+        // {
+        //     context.Remove(veggieSpecial);
+        // }
+        // context.SaveChanges();
+
+        // var products = context.Products
+        //                 .Where(p => p.Price < 10.00M)
+        //                 .OrderBy(p => p.Name);
+
+        // foreach (var p in products)
+        // {
+        //     Console.WriteLine($"id.....: {p.Id}");
+        //     Console.WriteLine($"Name...: {p.Name}");
+        //     Console.WriteLine($"Price..: {p.Price}");
+        //     Console.WriteLine(new string('-', 20));
+        // }
+
+
         using ContosoPizzaContext context = new ContosoPizzaContext();
 
-        var veggieSpecial = context.Products
-                                .Where(p => p.Name == "Veggie Special Pizza")
-                                .FirstOrDefault();
-
-        if (veggieSpecial is Product)
+        Product veggieSpecial = new Product()
         {
-            context.Remove(veggieSpecial);
-        }
+            Name = "Veggie Special Pizza",
+            Price = 12.99M
+        };
+        context.Products.Add(veggieSpecial);
+
+        Product deluxeMeat = new Product()
+        {
+            Name = "Deluxe Meat Pizza",
+            Price = 9.9M
+        };
+        context.Products.Add(deluxeMeat);
+
         context.SaveChanges();
 
-        var products = context.Products
-                        .Where(p => p.Price < 10.00M)
-                        .OrderBy(p => p.Name);
-
-        foreach (var p in products)
-        {
-            Console.WriteLine($"id.....: {p.Id}");
-            Console.WriteLine($"Name...: {p.Name}");
-            Console.WriteLine($"Price..: {p.Price}");
-            Console.WriteLine(new string('-', 20));
-        }
     }
 }
